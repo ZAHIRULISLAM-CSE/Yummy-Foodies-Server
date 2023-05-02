36911ffd248a4985737a7f815bd27f6d511ede26
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-05-03 01:26:56 +0600

    make chefinfo path for client side

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000;
+
+const chefInfoData=require("./chefinfo.json");
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/', (req, res) => {
+    res.send('Welcome To Yummy Foodies!')
+})
+
+app.get('/chefinfo', (req, res) => {
+    res.send(chefInfoData);
 })
 
 app.listen(port, () => {
