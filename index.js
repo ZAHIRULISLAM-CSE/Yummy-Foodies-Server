1aee32da86fe66a73a35f974941f429668b4588a
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-05-03 17:45:07 +0600

    modified json file and find items for each id

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(cors());
 
 const chefInfoData=require("./chefinfo.json");
-
+const chefReceipe=require("./chef_receipe.json");
 
 app.get('/', (req, res) => {
     res.send('Welcome To Yummy Foodies!')
@@ -14,6 +14,14 @@ app.get('/', (req, res) => {
 app.get('/chefinfo', (req, res) => {
     res.send(chefInfoData);
 })
+
+app.get('/chefReceipe/:id', (req, res) => {
+      const id =req.params.id;
+      const selectedData=chefReceipe.find(singleData => singleData.id == id);
+      res.send(selectedData);
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
